ef28a0b193c1202f96866930138962343fbe92ed
INTEGRATION\root <ancgate@yahoo.fr>
2018-04-24 12:40:16 -0400

    Add project files.

---
+++ CryptographyManager/CryptographyManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptographyManager
+{
+    public class CryptographyManager
+    {
+        public CryptographyManager()
+        {
+
+        }
+
+        public enum HashName
+        {
+            SHA1 = 1,
+            MD5 = 2,
+            SHA256 = 4,
+            SHA384 = 8,
+            SHA512 = 16
+        }
+
+        #region Encryption methods
+        public string Encrypt(string value)
+        {
+            return Encrypt(value, string.Empty);
+        }
+
+        public string Encrypt(string value, string key)
+        {
+            return Encrypt(value, key, string.Empty);
+        }
+
+        public string Encrypt(string value, string key, string iv)
+        {
+            string encryptValue = string.Empty;
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                try
+                {
+                    var encryptor = new AesManaged();
+                    
+                    var saltBytes = new UTF8Encoding().GetBytes(key);
+                    var rfc = new Rfc2898DeriveBytes(key, saltBytes);
+
+                    encryptor.Key = rfc.GetBytes(16);
+                    encryptor.IV = rfc.GetBytes(16);
+                    encryptor.BlockSize = 128;
+
+                    using (var encryptionStream = new MemoryStream())
+                    {
+                        using (var encrypt = new CryptoStream(encryptionStream, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            var utfD1 = UTF8Encoding.UTF8.GetBytes(value);
+                            encrypt.Write(utfD1, 0, utfD1.Length);
+                            encrypt.FlushFinalBlock();
+                            encrypt.Close();
+
+                            encryptValue = Convert.ToBase64String(encryptionStream.ToArray());
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error happened: " + ex.Message.ToString());
+                }
+            }
+
+            return encryptValue;
+        }
+        #endregion
+
+        #region Decryption methods
+        public string Decrypt(string value)
+        {
+            return Decrypt(value, string.Empty); 
+        }
+
+        public string Decrypt(string value, string key)
+        {
+            return Decrypt(value, key, string.Empty); 
+        }
+
+        public string Decrypt(string value, string key, string iv)
+        {
+            string decrptValue = string.Empty;
+            if (!string.IsNullOrEmpty(value))
+            {
+                value = value.Replace(" ", "+");
+                try
+                {
+
+                    var decryptor = new AesManaged();
+                    
+                    byte[] encryptedData = Convert.FromBase64String(value);
+
+                    byte[] saltBytes = new UTF8Encoding().GetBytes(key);
+                    var rfc = new Rfc2898DeriveBytes(key, saltBytes);
+
+                    decryptor.Key = rfc.GetBytes(16);
+                    decryptor.IV = rfc.GetBytes(16);
+                    decryptor.BlockSize = 128;
+
+                    using (var decryptionStream = new MemoryStream())
+                    {
+                        using (var decrypt = new CryptoStream(decryptionStream, decryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            try
+                            {
+                                decrypt.Write(encryptedData, 0, encryptedData.Length);
+                                decrypt.Flush();
+                                decrypt.Close();
+                            }
+                            catch (Exception innerEx)
+                            {
+                                Console.WriteLine("Error while decrypting: " + innerEx.Message.ToString());
+                            }
+                            var decryptedData = decryptionStream.ToArray();
+                            decrptValue = UTF8Encoding.UTF8.GetString(decryptedData, 0, decryptedData.Length);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //TODO: write log 
+                    Console.WriteLine("Error occurred: " + ex.Message.ToString());
+                }
+            }
+            return decrptValue;
+        }
+        #endregion
+    }
+}
---
+++ EncryptionTutorial/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace EncryptionTutorial
+{
+    class Program
+    {
+        static CryptographyManager.CryptographyManager cm = new CryptographyManager.CryptographyManager();
+        public const string m_LocalKey = "yourlocalkey";
+        public const string encryptionSalt = "yourlocalkey";
+
+        static void Main(string[] args)
+        {
+
+            int i = 300000;
+
+
+
+            System.Timers.Timer t = new System.Timers.Timer(4000);
+            t.Elapsed += (s, e) =>
+            {
+                var accountId = i;
+                var contactId = i;
+                string token = EncryptingToken(accountId, contactId);
+                DecryptingToken(token, encryptionSalt);
+                i += 2;
+            };
+            t.Start();
+
+            Console.ReadKey(); 
+        }
+
+        private static void DecryptingToken(string token, string encryptionSalt)
+        {
+
+            if (token != null)
+            {
+                Console.WriteLine("Token: " + token + Environment.NewLine);
+                string replaceToken = token.Replace("-", "/").Replace("_", "+"); 
+                    
+                    
+
+                int mod4 = replaceToken.Length % 4;
+                if (mod4 > 0)
+                {
+                    
+                    replaceToken += new string('=', 4 - mod4);
+                }
+                else
+                {
+                    replaceToken = token.Replace("-", "/").Replace("_", "+");
+                }
+
+                Console.WriteLine("Replace Token: " + replaceToken + Environment.NewLine);
+
+                Console.WriteLine("Length of the Token: " + replaceToken.Length + Environment.NewLine);
+                var uncovered = cm.Decrypt(replaceToken, encryptionSalt);
+                Console.WriteLine("Decrypted Result: " + uncovered);
+
+
+                var split = uncovered.Split(new[] { "Y-Y" }, StringSplitOptions.None);
+
+                var accountId = Convert.ToInt32(split[0]);
+                var contactId = Convert.ToInt32(split[1]);
+                var dateRequest = DateTime.ParseExact(split[2], "yyyyMMdd HHmmss", CultureInfo.GetCultureInfo("fr-CA"));
+
+                
+                
+                Console.WriteLine("AccountID: " + accountId);
+                Console.WriteLine("ContactID: " + contactId);
+                Console.WriteLine("DateRequest: " + dateRequest);
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine(Environment.NewLine);
+
+
+            }
+
+        }
+
+        private static string EncryptingToken (int accountId, int contactId) {
+
+
+            var dateNow = DateTime.Now.ToString("yyyyMMdd HHmmss", CultureInfo.GetCultureInfo("fr-CA"));
+            string rawToken = accountId.ToString() + "Y-Y" + contactId.ToString() + "Y-Y" + dateNow;
+
+            Console.WriteLine("Raw Token: " + rawToken + Environment.NewLine);
+            string token = cm.Encrypt(rawToken, m_LocalKey, encryptionSalt);
+            string replaceToken = token.Replace("/", "-").Replace("+","_").Replace("=",String.Empty);
+
+            Console.WriteLine("Encrypted Result: " + token + Environment.NewLine + "Length of Token: " + token.Length + Environment.NewLine);
+            Console.WriteLine("Replace Encrypted Result: " + replaceToken + Environment.NewLine + "Length of Token: " + replaceToken.Length + Environment.NewLine);
+
+            return replaceToken;
+
+        }
+
+    }
+}
